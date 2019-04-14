aca2815284ec43ef3d525b7c8d6873a5c79a886d
Declan Atkins <declanatkins@gmail.com>
2019-04-14 15:54:05 +0100

    Adds retrieval of isbn db image from html

--- isbn_to_image/isbn_lookup.py
+++ isbn_to_image/isbn_lookup.py
@@ -1,5 +1,5 @@
 import requests
-from lxml import etree as ET 
+from lxml import etree as ET
 
 
 ISBN_DB_URL = 'https://isbndb.com/book/'
@@ -20,6 +20,28 @@ def isbn_lookup(isbn_code):
         return response.content
 
 
+def isbn_db_html_parse(isbn_db_html):
+    '''
+    Takes in response html from isbn db and returns
+    the link to the isbn db image of the book's cover
+    page and a link to the amazon html page if it
+    exists
+    '''
+    parser = ET.HTMLParser(encoding='utf-8')
+    html_tree = ET.fromstring(isbn_db_html, parser=parser)
+    isbn_image_link = ''
+    amazon_link = ''
+    # Get the base image stored on isbn db
+    for elem in html_tree.iter():
+        if elem.tag == 'object':
+            attribs = dict(elem.attrib)
+            isbn_image_link = attribs['data']
+            # Only interested in the first one
+            break
+    return isbn_image_link, amazon_link
+    
+    
+
 if __name__ == '__main__':
     response = isbn_lookup(9780735219090)
-    print(response)
+    isbn_db_html_parse(response)
